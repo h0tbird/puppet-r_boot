9500eec0d955b73e2a31191827ed2e8a287f4a1a
Marc Villacorta <marc.villacorta@gmail.com>
2014-07-12 20:08:00 +0200

    Parametrize r_dnsmasq

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,17 @@
-class r_dnsmasq {
+class r_dnsmasq (
 
-  include ::dnsmasq
+  $dnsmasq = undef,
+
+) {
+
+  if $dnsmasq {
+    class { 'dnsmasq':
+      ensure        => $dnsmasq['ensure'],
+      enable        => $dnsmasq['enable'],
+      version       => $dnsmasq['version'],
+      effective_uid => $dnsmasq['effective_uid'],
+      effective_gid => $dnsmasq['effective_gid'],
+      dhcp_range    => $dnsmasq['dhcp_range'],
+    }
+  }
 }
