c0180d39b75511b5e33b9092de037a5326a262f8
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-23 23:39:17 +0200

    Update NFS module

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -1,9 +1,9 @@
 class r_boot::nfs inherits r_boot {
 
   if $nfs {
-    notify { 'NFS to do ...': }
-    # class { '::nfs':
-    #   foo => $nfs[''],
-    # }
+    class { '::nfs':
+      version => $nfs['version'],
+      enable  => $nfs['enable'],
+    }
   }
 }
