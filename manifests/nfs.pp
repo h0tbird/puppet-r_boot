fb615c775c6237492b4d1e3c1700aa3f3ff36c5a
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-28 13:37:39 +0200

    Populate /etc/exports

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -1,10 +1,20 @@
 class r_boot::nfs inherits r_boot {
 
   if $nfs {
+
     class { '::nfs':
       ensure  => $nfs['ensure'],
       enable  => $nfs['enable'],
       version => $nfs['version'],
+    }
+
+    file { '/etc/exports':
+      ensure  => present,
+      content => '/kickstart *(ro)',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      before  => Service['nfs-server'],
     }
   }
 }
