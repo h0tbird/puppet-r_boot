74812a117097db28dd96e6fc311c1e6cc645c52a
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-28 15:57:46 +0200

    Adding expots parameter

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -1,20 +1,11 @@
 class r_boot::nfs inherits r_boot {
 
   if $nfs {
-
     class { '::nfs':
       ensure  => $nfs['ensure'],
       enable  => $nfs['enable'],
       version => $nfs['version'],
-    }
-
-    file { '/etc/exports':
-      ensure  => present,
-      content => '/kickstart *(ro)',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      notify  => Service['nfs-server'],
+      exports => $nfs['exports'],
     }
   }
 }
