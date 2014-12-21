87fe0d327abd7eac6c6f83120f5f17662f5c2023
Marc Villacorta <marc.villacorta@gmail.com>
2014-12-21 19:33:31 +0100

    Remove PXELinux module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,10 @@
 class r_boot (
 
-  $pxelinux = undef,
-  $dnsmasq  = undef,
-  $nfs      = undef,
+  $dnsmasq = undef,
+  $nfs     = undef,
 
 ) {
 
-  contain "${module_name}::pxelinux"
   contain "${module_name}::dnsmasq"
   contain "${module_name}::nfs"
   contain "${module_name}::images"
--- manifests/pxelinux.pp
+++
@@ -1,8 +0,0 @@
-class r_boot::pxelinux inherits r_boot {
-
-  if $pxelinux {
-    class { '::pxelinux':
-      version => $pxelinux['version'],
-    }
-  }
-}
